c1e5b08acaddd3a0e5d6a042141489ebaab2e51b
Serhii Demchenko <svdemchenko@gmail.com>
2023-07-31 20:40:11 +0300

    update

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,7 +2,7 @@ import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 
-function Expenses(props) {
+const Expenses = (props) => {
   return (
     <Card className="expenses">
       {props.data.map((expense) => (
@@ -10,6 +10,6 @@ function Expenses(props) {
       ))}
     </Card>
   );
-}
+};
 
 export default Expenses;
