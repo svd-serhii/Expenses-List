b95cb19f422674d0d563c6c0969ede99c6b291e9
Serhii Demchenko <svdemchenko@gmail.com>
2023-08-01 00:01:57 +0300

    Update ExpenseItem.js

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,25 @@
+import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.data.title);
+
+  function clickHandler() {
+    setTitle("Updated!");
+    console.log(title);
+  }
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.data.date} />
       <div className="expense-item__description">
-        <h2>{props.data.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.data.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change title</button>
     </Card>
   );
 };
